676aac17ed7f15fcdeef948e65f9c81404596662
Dinesh Nanda <dannynanda76@gmail.com>
2020-03-12 21:41:02 -0400

    Identity Matrix

---
+++ Identity Matrix~/IdentityMatrix/src/main/java/identity_matrix/IdentityMatrix.java
@@ -0,0 +1,35 @@
+package identity_matrix;
+
+/**
+ *
+ * @author Dinesh Nanda
+ */
+public class IdentityMatrix {
+
+    public static void main(String[] args) {
+
+        int a[][] = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+
+        int count_one = 0;
+        int check_zero = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+
+                if (i == j) {
+                    if (a[i][j] == 1) {
+                        count_one++;
+                    }
+                } else if (a[i][j] != 0) {
+                    check_zero++;
+                }
+            }
+        }
+        if (count_one == a.length && check_zero == 0) {
+            System.out.println("Identity Matrix");
+        } else {
+            System.out.println("Not an Identity Matrix");
+        }
+    }
+
+}
